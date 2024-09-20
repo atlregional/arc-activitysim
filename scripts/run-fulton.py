7087d960209129fdb8e4ff9824f7dda32a60869d
Jeff Newman <jeff@driftless.xyz>
2024-09-20 10:54:53 -0500

    argparse

--- scripts/run-fulton.py
+++ scripts/run-fulton.py
@@ -1,3 +1,4 @@
+# ruff: noqa: E402
 import os
 
 # These environment variables are set to 1 to ensure that the model runs single-threaded
@@ -9,6 +10,7 @@ os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
 os.environ["NUMBA_NUM_THREADS"] = "1"
 
+import argparse
 import activitysim.abm  # register components # noqa: F401
 from pathlib import Path
 import sharrow
@@ -23,10 +25,15 @@ def local_path(dirname) -> str:
     return str(Path(__file__).parents[1].joinpath(dirname))
 
 
-def main(use_sharrow=False, n_households=1_000):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sharrow", help="use sharrow", action="store_true")
+    parser.add_argument("--households-sample-size", type=int, default=1000)
+    args = parser.parse_args()
 
     # These things change based on whether we are using sharrow or not.
-    if use_sharrow:
+    if args.sharrow:
+        print("sharrow activated")
         configs_dir = (
             local_path("configs_sh_compile"),
             local_path("configs"),
@@ -49,7 +56,7 @@ def main(use_sharrow=False, n_households=1_000):
     # for this test script, we will sample a limited number of households,
     # and run the model with only 2 processes.
     settings = {
-        "households_sample_size": n_households,
+        "households_sample_size": args.households_sample_size,
         "num_processes": 2,
         # user can modify other settings here if desired
     }
